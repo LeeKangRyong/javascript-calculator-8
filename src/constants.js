54aa547967889e984e66d9ec61749a5f74bba531
LeeKangRyong <gaiogo2@naver.com>
2025-10-17 15:59:08 +0900

    fix(constants): add [ERROR] infront of message

--- src/constants.js
+++ src/constants.js
@@ -4,10 +4,10 @@ export const INPUT_MESSAGE = {
 }
 
 export const ERROR_MESSAGE = {
-    'INVALID_INPUT' : 'No query input',
-    'INVALID_VALUE' : 'No values',
-    'INVALID_NUMBER' : 'Non-number exists',
-    'INVALID_POSITIVE' : 'Non-positive value exists'
+    'INVALID_INPUT' : '[ERROR] No query input',
+    'INVALID_VALUE' : '[ERROR] No values',
+    'INVALID_NUMBER' : '[ERROR] Non-number exists',
+    'INVALID_POSITIVE' : '[ERROR] Non-positive value exists'
 }
 
 export const SEPERATOR = {
