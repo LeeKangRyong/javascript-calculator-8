603e7ea7a822e35bcb7f5279e86a615c947cb80f
LeeKangRyong <gaiogo2@naver.com>
2025-10-17 13:41:41 +0900

    feat(constants): add seperator constants

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,19 @@
 export const INPUT_MESSAGE = {
     'INPUT' : '덧셈할 문자열을 입력해주세요.\n',
-    'RESULT' : '결과 : '
+    'RESULT' : '결과 :'
 }
 
 export const ERROR_MESSAGE = {
-    'INVALID_INPUT' : '[ERROR] No query input',
-    'INVALID_VALUE' : '[ERROR] No values',
-    'INVALID_NUMBER' : '[ERROR] Non-number exists',
-    'INVALID_POSITIVE' : '[ERROR] Non-positive value exists'
+    'INVALID_INPUT' : 'No query input',
+    'INVALID_VALUE' : 'No values',
+    'INVALID_NUMBER' : 'Non-number exists',
+    'INVALID_POSITIVE' : 'Non-positive value exists'
+}
+
+export const SEPERATOR = {
+    'DEFAULT_COMMA' : ',',
+    'DEFAULT_COLON' : ':',
+
+    'CUSTOM_START' : '//',
+    'CUSTOM_END' : '\n'
 }
