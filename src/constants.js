abe0242d7530db071299ca56b70e64b2b2ac3e39
LeeKangRyong <gaiogo2@naver.com>
2025-10-17 12:00:33 +0900

    feat(constants): add input and error message constants

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const INPUT_MESSAGE = {
+    'INPUT' : '덧셈할 문자열을 입력해주세요.\n',
+    'RESULT' : '결과 : '
+}
+
+export const ERROR_MESSAGE = {
+    'INVALID_INPUT' : '[ERROR] No query input',
+    'INVALID_VALUE' : '[ERROR] No values',
+    'INVALID_NUMBER' : '[ERROR] Non-number exists',
+    'INVALID_POSITIVE' : '[ERROR] Non-positive value exists'
+}
