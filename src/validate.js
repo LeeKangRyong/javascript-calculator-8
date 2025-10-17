cbb5dc0c361b2fab85ad3f9dc2243ddc39b14be2
LeeKangRyong <gaiogo2@naver.com>
2025-10-17 12:15:59 +0900

    feat(validate): add 5 error check functions

---
+++ src/validate.js
@@ -0,0 +1,19 @@
+export function isInvalid(query) {
+    return query === 'null' || query === 'undefined';
+}
+
+export function isEmpty(query) {
+    return query.length === 0;
+}
+
+export function isCustom(query) {
+    return query.startsWith('//');
+}
+
+export function isNumber(arr) {
+    return arr.every(v => typeof v === 'number');
+}
+
+export function isPositive(arr) {
+    return arr.every(v => +v > 0);
+}
